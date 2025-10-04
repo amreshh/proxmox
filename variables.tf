c82b1497986d2b267655001ea8dd9cbf56dd59fd
amreshh <20923769+amreshh@users.noreply.github.com>
2025-10-04 13:53:41 +0200

    chore(deps): update talos / cilium
    - Talos version update
    - Cilium version update

--- variables.tf
+++ variables.tf
@@ -5,9 +5,9 @@ variable "talos" {
     image   = string
   })
   default = {
-    version = "1.11.1"
-    iso     = "local:iso/talos_1.11.1.iso"
-    image   = "factory.talos.dev/metal-installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.11.1"
+    version = "1.11.2"
+    iso     = "local:iso/talos_1.11.2.iso"
+    image   = "factory.talos.dev/metal-installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.11.2"
   }
 }
 
@@ -37,7 +37,7 @@ variable "kubernetes_extra_manifests" {
 
 variable "cilium_version" {
   type        = string
-  default     = "1.18.1"
+  default     = "1.18.2"
   description = "Cilium version to provision"
 }
 

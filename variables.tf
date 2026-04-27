3ac985771e5a3cac4e2d3c0b921379542d07f8cd
amreshh <20923769+amreshh@users.noreply.github.com>
2026-04-27 09:47:37 +0200

    democratic-csi new networking zfs setup
    Signed-off-by: amreshh <20923769+amreshh@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -4,11 +4,6 @@ variable "talos" {
     iso     = string
     image   = string
   })
-  default = {
-    version = "1.12.7"
-    iso     = "local:iso/talos_1.12.7.iso"
-    image   = "factory.talos.dev/metal-installer/376567988ad370138ad8b2698212367b8edcb69b5fd68c80be1f2ec7d603b4ba:v1.12.7"
-  }
 }
 
 variable "github_token" {
@@ -18,26 +13,16 @@ variable "github_token" {
 
 variable "kubernetes_version" {
   type        = string
-  default     = "1.35.3"
   description = "Kubernetes version to provision"
 }
 
 variable "kubernetes_extra_manifests" {
-  type = list(string)
-  default = [
-    "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.5.1/config/crd/standard/gateway.networking.k8s.io_gatewayclasses.yaml",
-    "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.5.1/config/crd/standard/gateway.networking.k8s.io_gateways.yaml",
-    "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.5.1/config/crd/standard/gateway.networking.k8s.io_httproutes.yaml",
-    "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.5.1/config/crd/standard/gateway.networking.k8s.io_referencegrants.yaml",
-    "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.5.1/config/crd/standard/gateway.networking.k8s.io_grpcroutes.yaml",
-    "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.5.1/config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml"
-  ]
+  type        = list(string)
   description = "Extra kubernetes manifest to apply"
 }
 
 variable "cilium_version" {
   type        = string
-  default     = "1.19.3"
   description = "Cilium version to provision"
 }
 
@@ -46,10 +31,6 @@ variable "flux_version" {
     version      = string
     sync_version = string
   })
-  default = {
-    version      = "2.18.3"
-    sync_version = "1.14.5"
-  }
 }
 
 variable "controlplanes" {
@@ -63,18 +44,6 @@ variable "controlplanes" {
     mac_addr     = string
     ip_addr      = string
   }))
-  default = {
-    controlplane1 = {
-      cluster_name = "talos"
-      name         = "k8s-controlplane-1"
-      vm_id        = 101
-      memory       = 8192 # MiB
-      cores        = 2
-      disk_size    = 10 # GiB
-      mac_addr     = "02:00:00:00:00:01"
-      ip_addr      = "192.168.10.20"
-    }
-  }
 }
 
 variable "workers" {
@@ -88,26 +57,4 @@ variable "workers" {
     mac_addr     = string
     ip_addr      = string
   }))
-  default = {
-    worker1 = {
-      cluster_name = "talos"
-      name         = "k8s-worker-1"
-      vm_id        = 102
-      memory       = 16384 # MiB
-      cores        = 4
-      disk_size    = 100 # GiB
-      mac_addr     = "02:00:00:00:00:02"
-      ip_addr      = "192.168.10.21"
-    }
-    worker2 = {
-      cluster_name = "talos"
-      name         = "k8s-worker-2"
-      vm_id        = 103
-      memory       = 16384 # MiB
-      cores        = 4
-      disk_size    = 100 # GiB
-      mac_addr     = "02:00:00:00:00:03"
-      ip_addr      = "192.168.10.22"
-    }
-  }
 }

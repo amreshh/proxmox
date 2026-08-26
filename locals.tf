66f3bbf168f913f4a63ea79bec1371390157234e
amreshh <20923769+amreshh@users.noreply.github.com>
2026-08-26 16:12:44 +0200

    dev moved to isolated network
    Signed-off-by: amreshh <20923769+amreshh@users.noreply.github.com>

--- locals.tf
+++ locals.tf
@@ -2,13 +2,13 @@ locals {
   settings = {
     dev = {
       talos = {
-        version      = "1.13.8"
-        iso          = "local:iso/talos_1.13.8_qemu.iso"
-        image        = "factory.talos.dev/metal-installer/376567988ad370138ad8b2698212367b8edcb69b5fd68c80be1f2ec7d603b4ba:v1.13.8"
-        vm_disk      = "/dev/vda"
+        version      = "1.13.9"
+        iso          = "local:iso/talos_1.13.9.iso"
+        image        = "factory.talos.dev/metal-installer/9ed5fecdacb36b5c5427b87d409f1065cfb2df69b0f71c58b868d9d466d8dab3:v1.13.9"
+        vm_disk      = "/dev/nvme0n1"
         cluster_name = "dev"
-        time_servers = ["192.168.20.1"]
-        dns_servers  = ["192.168.122.1/32"]
+        time_servers = ["10.10.10.1"]
+        dns_servers  = ["10.10.10.1/32"]
       }
       kubernetes_version = "1.36.2"
       kubernetes_extra_manifests = [
@@ -17,9 +17,9 @@ locals {
         "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.6.1/config/crd/standard/gateway.networking.k8s.io_httproutes.yaml",
         "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.6.1/config/crd/standard/gateway.networking.k8s.io_referencegrants.yaml",
         "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.6.1/config/crd/standard/gateway.networking.k8s.io_grpcroutes.yaml",
-        "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.6.1/config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml"
+        "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.6.1/config/crd/standard/gateway.networking.k8s.io_tlsroutes.yaml"
       ]
-      cilium_version = "1.20.0"
+      cilium_version = "1.20.1"
       flux_version = {
         version      = "2.19.0"
         sync_version = "1.15.0"
@@ -27,23 +27,23 @@ locals {
       controlplanes = {
         controlplane1 = {
           name      = "k8s-controlplane-1"
-          vm_id     = 101
+          vm_id     = 100
           memory    = 8192
           cores     = 2
           disk_size = 50
           mac_addr  = "52:54:00:62:d5:d2"
-          ip_addr   = "192.168.122.52"
+          ip_addr   = "10.10.10.20"
         }
       }
       workers = {
         worker1 = {
           name      = "k8s-worker-1"
-          vm_id     = 102
+          vm_id     = 101
           memory    = 16384
           cores     = 4
           disk_size = 100
           mac_addr  = "52:54:00:04:51:28"
-          ip_addr   = "192.168.122.251"
+          ip_addr   = "10.10.10.21"
         }
       }
     }
@@ -65,7 +65,7 @@ locals {
         "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.5.1/config/crd/standard/gateway.networking.k8s.io_httproutes.yaml",
         "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.5.1/config/crd/standard/gateway.networking.k8s.io_referencegrants.yaml",
         "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.5.1/config/crd/standard/gateway.networking.k8s.io_grpcroutes.yaml",
-        "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.5.1/config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml"
+        "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.5.1/config/crd/standard/gateway.networking.k8s.io_tlsroutes.yaml"
       ]
       cilium_version = "1.19.4"
       flux_version = {
